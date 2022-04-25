80bdc399c97b2febc309928545c2d31da54cf5c4
Andrew Phillips <aphillips15@gmail.com>
2022-04-24 21:46:29 -0700

    started DOM js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,22 @@
-const entrees = data.recipes.filter(recipes => recipes.recipeCategory == "Entree");
-const desserts = data.recipes.filter(recipes => recipes.recipeCategory == "Dessert");
 
-const vegetarian = data.recipes.filter(recipes => recipes.vegetarian == true);
+//Defining Filters
+    const entrees = data.recipes.filter(recipes => recipes.recipeCategory == "Entree");
+    const desserts = data.recipes.filter(recipes => recipes.recipeCategory == "Dessert");
+    const vegetarian = data.recipes.filter(recipes => recipes.vegetarian == true);
+
+//DOM Script
+const ingredients = document.querySelector('#ingredients');
+const instructions = document.querySelector('#instructions');
+
+//functions
+function addIngredients(){
+    //function exists to add a list of ingredients to the ingredients Div
+    const ingredients = document.querySelector('#ingredients');
+    const p = document.createElement('p');
+    p.textContent = `Test Ingredients`;
+    ingredients.appendChild(p)
+}
+
+function addInstructions(){
+
+}
