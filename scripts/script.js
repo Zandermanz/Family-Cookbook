906a241df32c3c8eefe2efcdb482a68bf96bf532
Andrew Phillips <aphillips15@gmail.com>
2022-04-27 22:34:09 -0700

    Remove data. Update addIngredient func

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,17 @@
-
 //Defining Filters
-    const entrees = data.recipes.filter(recipes => recipes.recipeCategory == "Entree");
-    const desserts = data.recipes.filter(recipes => recipes.recipeCategory == "Dessert");
-    const vegetarian = data.recipes.filter(recipes => recipes.vegetarian == true);
+    const entrees = recipes.filter(recipes => recipes.recipeCategory == "Entree");
+    const desserts = recipes.filter(recipes => recipes.recipeCategory == "Dessert");
+    const vegetarian = recipes.filter(recipes => recipes.vegetarian == true);
 
 //DOM Script
     const ingredients = document.getElementById('ingredient');
     const instructions = document.getElementById('instruction');
     const container = document.getElementById('container');
 
-
-//functions
-
+//Functions
 //Adds a list of ingredients to the ingredients Div
-function addIngredients(){
-    let ingredientList = document.getElementById('ingredientList'); //sets ingredient UL id
-    let listItem = document.createElement('li'); //setting list item as new li element
-    listItem.textContent = 'Test Ingredient'; //setting text content of li element
-    ingredientList.appendChild(listItem); //appending listItem to ul
+function addIngredients(ingredientArray){
+    arrayToHTMLList(ingredientArray, ingredientList);
 }
 
 function addInstructions(){
@@ -30,7 +24,7 @@ function addInstructions(){
 //take recipe title and return array of recipe ingredients
 //unfinished function--------------
 function getRecipeIngredients(title){
-    let recipe = data.recipes.filter(recipes =>{ 
+    let recipe = recipes.filter(recipes =>{ 
         if(recipes.title == title) {
             console.log(recipes.recipeIngredient) //logs correct array
             return recipes.recipeIngredient} // does not
@@ -40,10 +34,12 @@ function getRecipeIngredients(title){
     return recipe;
 }
 
-//takes array and list ID and pushes array items to list.
-const arrayToHTMLList = (arr, listID) =>
+//takes array and list ID as parameters, inserts <li> html elements for array
+function arrayToHTMLList(arr, listID) {
     document.querySelector(`#${listID}`).innerHTML += arr
     .map(item => `<li>${item}</li>`)
     .join('');
-
-// example code :arrayToHTMLList(['item 1', 'item 2'], 'myListID');
+}
+// example code :
+// arrayToHTMLList(['item 1', 'item 2'], 'myListID');
+// arrayToHTMLList(recipes[0].recipeIngredient, 'instructionList');
